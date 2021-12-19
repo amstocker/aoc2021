3b2b2970985dd78b3f01b0da037c884c10ddd5f8
Andrew Stocker <sleepyams@gmail.com>
2021-12-18 21:46:07 -0700

    day2

---
+++ day2.py
@@ -0,0 +1,18 @@
+with open("day2_input.txt") as f:
+    data = [(l[0], int(l[1])) for l in map(lambda s: s.split(), f.read().strip().splitlines())]
+
+# part 1
+x = 0; y = 0
+for s, d in data:
+    if s == "up":           y -= d
+    elif s == "down":       y += d
+    elif s == "forward":    x += d
+print(x * y)
+
+# part 2
+a = 0; x = 0; y = 0
+for s, d in data:
+    if s == "up":           a -= d
+    elif s == "down":       a += d
+    elif s == "forward":    x += d; y += (a * d)
+print(x * y)
