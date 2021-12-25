882ddfa1bea92032e125115b20b83627a3595345
Andrew Stocker <sleepyams@gmail.com>
2021-12-25 10:53:04 -0700

    day6

---
+++ day6.py
@@ -0,0 +1,21 @@
+import functools
+
+with open("day6_input.txt") as f:
+    data = list(map(int, f.read().strip().split(',')))
+
+
+@functools.lru_cache
+def simulate(x, n):
+    if n <= x:
+        return 1
+    else:
+        return simulate(7, n - x) + simulate(9, n - x)
+
+def simulate_list(data, n):
+    return sum([simulate(x, n) for x in data])
+
+# part 1
+print(simulate_list(data, 80))
+
+# part 2
+print(simulate_list(data, 256))
