08452786aa76ac9fe135426c16ac40a895984dfd
Andrew Stocker <sleepyams@gmail.com>
2021-12-26 07:13:31 -0700

    day 7 update

--- day7.py
+++ day7.py
@@ -1,8 +1,11 @@
 with open("day7_input.txt") as f:
     data = [int(s) for s in f.read().strip().split(',')]
 
+d1 = lambda x, y: abs(x - y)
+d2 = lambda x, y: abs(x - y) * (abs(x - y) + 1) * (1/2)
+
 # part 1
-print(min(sum(abs(x - y) for y in data) for x in range(min(data), max(data) + 1)))
+print(min(sum(d1(x, y) for y in data) for x in range(min(data), max(data) + 1)))
 
 # part 2
-print(min(sum(int(abs(x - y)*(abs(x - y) + 1)/2) for y in data) for x in range(min(data), max(data) + 1)))
+print(min(sum(d2(x, y) for y in data) for x in range(min(data), max(data) + 1)))
