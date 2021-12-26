ad9711418ce6bc10e474ad615ad1eb516489f88f
Andrew Stocker <sleepyams@gmail.com>
2021-12-26 07:27:43 -0700

    Update day6.py

--- day6.py
+++ day6.py
@@ -4,12 +4,12 @@ with open("day6_input.txt") as f:
     data = list(map(int, f.read().strip().split(',')))
 
 @lru_cache
-def simulate(x, n):
+def fish(x, n):
     if n <= x:  return 1
-    else:       return simulate(7, n - x) + simulate(9, n - x)
+    else:       return fish(7, n - x) + fish(9, n - x)
 
 # part 1
-print(sum(simulate(x, 80) for x in data))
+print(sum(fish(x, 80) for x in data))
 
 # part 2
-print(sum(simulate(x, 256) for x in data))
+print(sum(fish(x, 256) for x in data))
